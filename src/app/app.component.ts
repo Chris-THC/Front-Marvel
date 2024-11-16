3135551ca522772186e6f96cc7363f3b9931e3c0
Chris CR <chris_49git@outlook.com>
2024-11-16 17:43:55 -0600

    I added Tailwind CSS into the project 'n' add a Modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HttpClientModule],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ApiCallService } from '../core/services/api-call.service';
-import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../modal/modal.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  
   characters: any[] = [];
   loading: boolean = true;
 
@@ -24,7 +25,14 @@ export class HomeComponent implements OnInit {
     this.apiCallService.getCharactersList().subscribe({
       next: (response) => {
         console.log('Characters loaded:', response.data.results);
-        this.characters = response.data.results; // Asignar la respuesta al array de personajes
+        this.characters = response.data.results.map((character: any) => {
+          return {
+            id: character.id,
+            name: character.name,
+            thumbnail: `${character.thumbnail.path}.${character.thumbnail.extension}`,
+          };
+        });
+
         this.loading = false; // Cambiar el estado de carga a false
       },
       error: (error) => {
@@ -32,5 +40,15 @@ export class HomeComponent implements OnInit {
         this.loading = false; // Cambiar el estado de carga a false en caso de error
       },
     });
+  }
+  
+  isModalVisible = false;
+  modalContent = 0;
+
+  openModal(characterId: number) {
+    this.modalContent = characterId;
+    this.isModalVisible = true;
+    console.log(this.isModalVisible);
+    
   }
 }
